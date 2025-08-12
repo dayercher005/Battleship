c623b76f9414a643d622e5199375154ca74e5338
Dayer <93074807+dayercher005@users.noreply.github.com>
2025-08-12 21:21:12 +0800

    Rendering vertical and horizontal positioning of Ship Classes

--- src/__tests__/testGameBoard.test.js
+++ src/__tests__/testGameBoard.test.js
@@ -3,13 +3,35 @@ import {GameBoard} from "../assets/GameBoard.js"
 const newGrid = new GameBoard();
 
 test("Creating GameBoard Grid", ()=> {
-    expect(newGrid.createBoard()).toEqual(
-        [[0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0],]
+    expect(newGrid.gameGrid).toEqual(
+        [[0, 0, 0, 0, 0, 0, 0], 
+         [0, 0, 0, 0, 0, 0, 0], 
+         [0, 0, 0, 0, 0, 0, 0], 
+         [0, 0, 0, 0, 0, 0, 0], 
+         [0, 0, 0, 0, 0, 0, 0], 
+         [0, 0, 0, 0, 0, 0, 0], 
+         [0, 0, 0, 0, 0, 0, 0]]
     )
+});
+
+test("GameBoards should be able to place ships at specific coordinates by calling the Ship class.", () => {
+    expect(newGrid.placeShips(1, 1, "horizontal")).toEqual(
+        [[0, 0, 0, 0, 0, 0, 0], 
+         [0, 1, 1, 1, 0, 0, 0], 
+         [0, 0, 0, 0, 0, 0, 0], 
+         [0, 0, 0, 0, 0, 0, 0], 
+         [0, 0, 0, 0, 0, 0, 0], 
+         [0, 0, 0, 0, 0, 0, 0], 
+         [0, 0, 0, 0, 0, 0, 0]]
+    );
+
+    expect(newGrid.placeShips(2, 2, "vertical")).toEqual(
+        [[0, 0, 0, 0, 0, 0, 0], 
+         [0, 1, 1, 1, 0, 0, 0], 
+         [0, 0, 1, 0, 0, 0, 0], 
+         [0, 0, 1, 0, 0, 0, 0], 
+         [0, 0, 1, 0, 0, 0, 0], 
+         [0, 0, 0, 0, 0, 0, 0], 
+         [0, 0, 0, 0, 0, 0, 0]]
+    );
 });
--- src/assets/GameBoard.js
+++ src/assets/GameBoard.js
@@ -1,29 +1,42 @@
+import {Ship} from "./Ship.js"
+
 export {GameBoard};
 
 class GameBoard{
 
+
+
     constructor(){
         this.height = 7;
         this.width = 7;
-    }
-
-    createBoard(){
-        const gameGrid = []
+        this.gameGrid = [];
         for(let height = 0; height < this.height; height++){
-            gameGrid[height] = [];
+            this.gameGrid[height] = [];
             for(let width = 0; width < this.height; width++){
-                gameGrid[height].push(0);
+                this.gameGrid[height].push(0);
             }
         }
-
-        return gameGrid;
     }
 
-    placeShips(){
 
+    placeShips(coordinateX, coordinateY, direction){
+        const newShip = new Ship(3, 0);
+
+        if (direction === "horizontal"){
+            for (let i = 0; i < newShip.length; i++){
+                this.gameGrid[coordinateX][coordinateY + i] = 1;
+            }
+            
+        } else if (direction === "vertical"){
+            for (let i = 0; i < newShip.length; i++){
+                this.gameGrid[coordinateX + i][coordinateY] = 1;
+            }
+    
+        }
+        return this.gameGrid;
     }
 
-    receiveAttack(coordinates){
+    receiveAttack(coordinateX, coordinateY){
 
     }
 
