0ef918833ef4d6c34f6146179e755677bf9cb213
Dayer <dayerxr.cher@gmail.com>
2025-08-17 18:40:15 +0800

    constructing tests for gameboard functions

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,3 +1,42 @@
+const {GameController} = require('../assets/App.js');
+const {GameBoard} = require('../assets/GameBoard.js');
+const {Ship} = require('../assets/Ship.js');
 
+const newBoard = new GameBoard;
+const newShip = new Ship(4, 0, 1);
+const LongerShip = new Ship(5, 0, 2)
 
+test("", () => {
+    expect(newBoard.placeShips(newShip, "horizontal", 3, 4)).toEqual(
+        [[null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null],
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, 1, 1, 1, 1, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null]]
+
+    );
+
+    expect(newBoard.placeShips(LongerShip, "horizontal", 3, 3)).toEqual(
+        false
+    );
+
+    expect(newBoard.placeShips(LongerShip, "horizontal", 0, 0)).toEqual(
+        [[2, 2, 2, 2, 2, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null],
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, 1, 1, 1, 1, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null]]
+    )
+
+});
 
