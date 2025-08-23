a9045fe921dde5ad93e629f322b2923956c86c26
Dayer <93074807+dayercher005@users.noreply.github.com>
2025-08-24 00:34:40 +0800

    Testing JS functions for UI, Logic and GameBoard files

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,14 +2,68 @@ const {GameController} = require('../assets/App.js');
 
 const newGame = GameController();
 
-test('HumanRandomPlacementController() should generate a random board filled with Ship Objects', () => {
-    expect(newGame.HumanRandomPlacementController()).toEqual(
-        
+test('ComputerBoardCreation() should generate a 10 x 10 grid.', () => {
+    expect(newGame.ComputerBoardCreation()).toEqual(
+        [[null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null]]
+    );
+});
+
+test('HumanBoardCreation() should generate a 10 x 10 grid.', () => {
+    expect(newGame.HumanBoardCreation()).toEqual(
+        [[null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null]]
+    );
+});
+
+test("HumanShipPlacementController() should place the given ship into their respective coordinate provided its valid", () => {
+    expect(newGame.HumanShipPlacementController(newGame.PatrolBoat, "vertical", 1, 1)).toEqual(
+        [[null, null, null, null, null, null, null, null, null, null], 
+        [null, {"hitNumber": 0, "length": 2, "sunk": false}, null, null, null, null, null, null, null, null], 
+        [null, {"hitNumber": 0, "length": 2, "sunk": false}, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null]]
     )
 })
 
-test('ComputerAttackController() should generate a board filled both previous ships and computer attacks', () => {
-    expect(newGame.ComputerAttackController()).toEqual(
-        
-    )
-})
+test("HumanAttackController() should attack the given coordinate, return None if there is no ship and hitShip if there is a ship.", () => {
+    expect(newGame.HumanAttackController(0, 0)).toEqual(
+        [["None", null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null]]
+    );
+
+});
+
+test("GameRestartController() should restart the game in the console by resetting the game board for both players.", () => {
+    expect(newGame.GameRestartController()).toEqual(false)
+});
+
--- src/__tests__/GameBoard.test.js
+++ src/__tests__/GameBoard.test.js
@@ -22,15 +22,15 @@ test("placeShips() method should place the specified ship in the correct coordin
 test("receiveAttack() method should evaluate the current GameBoard status and reflect the new hit coordinates.", () => {
     expect(newGameBoard.receiveAttack(3, 4)).toEqual(
         [[null, null, null, null, null, null, null, null, null, null], 
-         [null, null, null, null, null, null, null, null, null, null], 
-         [null, null, null, null, null, null, null, null, null, null], 
-         [null, null, null, null, "hitShip", {"hitNumber": 1, "length": 3, "sunk": false}, {"hitNumber": 1, "length": 3, "sunk": false}, null, null, null], 
-         [null, null, null, null, null, null, null, null, null, null], 
-         [null, null, null, null, null, null, null, null, null, null], 
-         [null, null, null, null, null, null, null, null, null, null], 
-         [null, null, null, null, null, null, null, null, null, null], 
-         [null, null, null, null, null, null, null, null, null, null], 
-         [null, null, null, null, null, null, null, null, null, null]]
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, "hitShip", {"hitNumber": 1, "length": 3, "sunk": false}, {"hitNumber": 1, "length": 3, "sunk": false}, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null]]
     );
 
     expect(newGameBoard.receiveAttack(0, 0)).toEqual(
@@ -45,4 +45,40 @@ test("receiveAttack() method should evaluate the current GameBoard status and re
          [null, null, null, null, null, null, null, null, null, null], 
          [null, null, null, null, null, null, null, null, null, null]]
     );
+
+
+    expect(newGameBoard.receiveAttack(3, 5)).toEqual(
+        [["None", null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, "hitShip", "hitShip", {"hitNumber": 2, "length": 3, "sunk": false}, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null]]
+    );
+});
+
+
+test("gameEnd() method should return false if all ships have sunk and true if all ships have not sunk.", () => {
+    expect(newGameBoard.gameEnd()).toEqual(true);
+
+    expect(newGameBoard.receiveAttack(3, 6)).toEqual(
+        [["None", null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, "hitShip", "hitShip", "hitShip", null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null], 
+         [null, null, null, null, null, null, null, null, null, null]]
+    );
+
+    expect(newGameBoard.shipArmy).toEqual([{"hitNumber": 3, "length": 3, "sunk": true}]);
+
+    expect(newGameBoard.gameEnd()).toEqual(false);
 });
